7760f0a0cb53cb6ab58636ec0bdd3c6267fed4da
Fikayo <fikfikky@gmail.com>
2021-02-22 22:18:55 +0100

    Initial commit

---
+++ site-data-manager/schemas/siteinfo.js
@@ -0,0 +1,17 @@
+export default {
+    name: "siteinfo",
+    title: "Site Information",
+    type : "document",
+    fields : [
+        {
+            name: "sitename",
+            title: "Website Name",
+            type: "string"
+        },
+        {
+            name: "description",
+            title: "Description",
+            type: "text"
+        }
+    ]
+}
---
+++ site-data-manager/schemas/topics.js
@@ -0,0 +1,17 @@
+export default {
+    name: "topics",
+    title: "Site Topics",
+    type : "document",
+    fields : [
+        {
+            name: "title",
+            title: "Title",
+            type: "string"
+        },
+        {
+            name: "description",
+            title: "Description",
+            type: "text"
+        }
+    ]
+}
